dc871a01ecee412fd45d45d2b2445c116aa52d5c
unknown <qgaillot@aeponyx.com>
2018-05-11 16:15:02 -0400

    Export de polygones avec/sans curve, pie, torus, circle

--- PolygonCSV/PolygonToCSV.c
+++ PolygonCSV/PolygonToCSV.c
@@ -59,7 +59,6 @@ void AddBox(LObject object, FILE * myFile, LFile pFile  )
 	Add( rect.x0, rect.y1, myFile, pFile );
 	Add( rect.x1, rect.y1, myFile, pFile );
 	Add( rect.x1, rect.y0, myFile, pFile );
-	LUpi_LogMessage(LFormat("NEW BOX Polygon without curves\n"));
 }
 
 void AddPolygon(LObject object, FILE * myFile, LFile pFile  )
@@ -69,6 +68,16 @@ void AddPolygon(LObject object, FILE * myFile, LFile pFile  )
 	{
 		Add( LVertex_GetPoint(currentVertex).x, LVertex_GetPoint(currentVertex).y, myFile, pFile );
 	}
+}
+
+void AddCurve(FILE* myFile, LFile pFile, double x, double y, int type, LCoord cx, LCoord cy, double r, double r2, double start, double stop, LArcDirection dir)
+{
+	char str[4];
+	if(dir == CW)
+		strcpy(str, "CW");
+	else
+		strcpy(str, "CCW");
+	fprintf(myFile, "%f,%f,%d,%f,%f,%f,%f,%f,%f,%s\n", (float)LFile_IntUtoMicrons(pFile, x), (float)LFile_IntUtoMicrons(pFile, y),type,(float)LFile_IntUtoMicrons(pFile, cx),(float)LFile_IntUtoMicrons(pFile, cy),(float)LFile_IntUtoMicrons(pFile, r),(float)LFile_IntUtoMicrons(pFile, r2),(float)start,(float)stop,str);
 }
 
 void PolygonToCSV(void)
@@ -163,21 +172,7 @@ void PolygonToCSV(void)
 				LPoint ptCenter = LCircle_GetCenter(pObj);
 				LCoord nRadius = LCircle_GetRadius(pObj);
 
-				#ifdef ALGORITHM_A
-							double dThetaStep = 2*acos(1 - (double)grid.manufacturing_grid_size / nRadius / 10);
-							for (double dTheta = 0; dTheta < 2.0 * M_PI; dTheta += dThetaStep )
-								Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-				#else
-							int n = 2.0 * M_PI * (double)nRadius / dArcSegLen;
-							if ( n < 8 )
-								n = 8;
-							double dThetaStep = 2.0 * M_PI / n;
-							for ( int i = 0; i < n; i++ )
-							{
-								double dTheta = i * dThetaStep;
-								Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-							}
-				#endif
+				AddCurve(myFile, pFile, 0, 0, 2, ptCenter.x, ptCenter.y, nRadius, 0, 0, 0, CCW);
 			}
 
 			else if (LObject_GetGeometry(pObj) != LCurved)
@@ -195,17 +190,16 @@ void PolygonToCSV(void)
 			switch( LObject_GetShape(pObj) )
 			{
 				case LPolygon:
+				{
 	//				LDialog_MsgBox("Found a curved polygon");
 					for(LVertex pVert = LObject_GetVertexList(pObj); pVert != NULL; pVert = LVertex_GetNext(pVert))
 					{
 	//					char msg[200];
 	//					sprintf(msg, "Found a vertex (%ld, %ld)", LVertex_GetPoint(pVert).x, LVertex_GetPoint(pVert).y);
 	//					LDialog_MsgBox(msg);
-						Add( LVertex_GetPoint(pVert).x, LVertex_GetPoint(pVert).y, myFile, pFile );
 						if ( ! LVertex_HasCurve(pVert))
 						{
-							cpt++;
-							fclose(myFile);
+							Add( LVertex_GetPoint(pVert).x, LVertex_GetPoint(pVert).y, myFile, pFile );
 							continue;
 						}
 						
@@ -216,133 +210,18 @@ void PolygonToCSV(void)
 						LCoord nRadius;
 						LVertex_GetCurveEX(pObj, pVert, &ptCenter, &nRadius, &ptStart, &ptEnd, &Dir);
 
-						double dStartAngle = atan2( ptStart.y - ptCenter.y, ptStart.x - ptCenter.x );
-						double dStopAngle = atan2( ptEnd.y - ptCenter.y, ptEnd.x - ptCenter.x );
-
-	//					sprintf(msg, "Found a curved vertex center (%ld, %ld) start = %g end = %g", ptCenter.x, ptCenter.y, dStartAngle, dStopAngle);
-	//					LDialog_MsgBox(msg);
-
-						if ( Dir == CCW )
-						{
-							// ensure dEndAngle > dStartAngle
-							while (dStopAngle < dStartAngle)
-								dStopAngle += 2.0 * M_PI;
-
-	#ifdef ALGORITHM_A
-							Add( ptCenter.x + nRadius * cos( dStartAngle ), ptCenter.y + nRadius * sin( dStartAngle ), myFile, pFile );
-							double dThetaStep = 2*acos(1 - (double)grid.manufacturing_grid_size / nRadius / 10);
-							for (double dTheta = dStartAngle; dTheta < dStopAngle; dTheta += dThetaStep )
-								Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-							Add( ptCenter.x + nRadius * cos( dStopAngle ), ptCenter.y + nRadius * sin( dStopAngle ), myFile, pFile );
-	#else
-							// insert n vertices
-							int n = Round( (dStopAngle - dStartAngle) * nRadius / dArcSegLen );
-							double dThetaStep = (dStopAngle - dStartAngle) / (n + 1);
-
-	//						sprintf(msg, "CCW verts = %d", n);
-	//						LDialog_MsgBox(msg);
-
-							Add( ptCenter.x + nRadius * cos( dStartAngle ), ptCenter.y + nRadius * sin( dStartAngle ), myFile, pFile );
-							for ( int i = 1; i <= n; i++ )
-							{
-								double dTheta = dStartAngle + i * dThetaStep;
-								Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-							}
-	#endif
-						}
-						else
-						{
-							// Dir == CW
-
-							// ensure dStartAngle > dEndAngle
-							while (dStartAngle < dStopAngle)
-								dStartAngle += 2.0 * M_PI;
-
-	#ifdef ALGORITHM_A
-							Add( ptCenter.x + nRadius * cos( dStartAngle ), ptCenter.y + nRadius * sin( dStartAngle ), myFile, pFile );
-							double dThetaStep = 2*acos(1 - (double)grid.manufacturing_grid_size / nRadius / 10);
-							for (double dTheta = dStartAngle; dTheta > dStopAngle; dTheta -= dThetaStep )
-								Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-							Add( ptCenter.x + nRadius * cos( dStopAngle ), ptCenter.y + nRadius * sin( dStopAngle ), myFile, pFile );
-	#else
-							// insert n vertices
-							int n = Round( (dStartAngle - dStopAngle) * nRadius / dArcSegLen);
-							double dThetaStep = (dStartAngle - dStopAngle) / (n + 1);
-
-	//						sprintf(msg, "CW verts = %d", n);
-	//						LDialog_MsgBox(msg);
-
-							Add( ptCenter.x + nRadius * cos( dStartAngle ), ptCenter.y + nRadius * sin( dStartAngle ), myFile, pFile );
-							for ( int i = 1; i <= n; i++ )
-							{
-								double dTheta = dStartAngle - i * dThetaStep;
-								Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-							}
-	#endif
-						}
+						AddCurve(myFile, pFile, LVertex_GetPoint(pVert).x, LVertex_GetPoint(pVert).y, 1, ptCenter.x, ptCenter.y, nRadius, 0, 0, 0, Dir);
 					}
+					cpt++;
+					fclose(myFile);
 					break;
-
+				}
 				case LTorus:
 				{
 					LTorusParams TorusParams;
 					LTorus_GetParams(pObj, &TorusParams);
-					double dStartAngle = TorusParams.dStartAngle * M_PI / 180.0;
-					double dStopAngle = TorusParams.dStopAngle * M_PI / 180.0;
-					while (dStopAngle < dStartAngle)
-						dStopAngle += 2.0 * M_PI;
-	//				LDialog_MsgBox("Found a torus");
-	//				char msg[200];
-	//				sprintf(msg, "Start = %g Stop = %g", dStartAngle, dStopAngle);
-	//				LDialog_MsgBox(msg);
 
-					// draw the polygon in CW direction, from start/inner
-
-					LPoint ptCenter = TorusParams.ptCenter;
-					LCoord nRadius = TorusParams.nInnerRadius;
-
-
-	#ifdef ALGORITHM_A
-					Add( ptCenter.x + nRadius * cos( dStartAngle ), ptCenter.y + nRadius * sin( dStartAngle ), myFile, pFile );
-					double dThetaStep = 2*acos(1 - (double)grid.manufacturing_grid_size / nRadius / 10);
-					for (double dTheta = dStartAngle; dTheta < dStopAngle; dTheta += dThetaStep )
-						Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-					Add( ptCenter.x + nRadius * cos( dStopAngle ), ptCenter.y + nRadius * sin( dStopAngle ), myFile, pFile );
-
-					nRadius = TorusParams.nOuterRadius;
-					dThetaStep = 2*acos(1 - (double)grid.manufacturing_grid_size / nRadius / 10);
-
-					Add( ptCenter.x + nRadius * cos( dStopAngle ), ptCenter.y + nRadius * sin( dStopAngle ), myFile, pFile );
-					for (double dTheta = dStopAngle; dTheta > dStartAngle; dTheta -= dThetaStep )
-						Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-					Add( ptCenter.x + nRadius * cos( dStartAngle ), ptCenter.y + nRadius * sin( dStartAngle ), myFile, pFile );
-	#else
-
-					int n = Round( (dStopAngle - dStartAngle) * nRadius / dArcSegLen );
-					if ( n < 8 )
-						n = 8;
-					double dThetaStep = (dStopAngle - dStartAngle) / n;
-
-					Add( ptCenter.x + nRadius * cos( dStartAngle ), ptCenter.y + nRadius * sin( dStartAngle ), myFile, pFile );
-					for ( int i = 1; i <= n; i++ )
-					{
-						double dTheta = dStartAngle + i * dThetaStep;
-						Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-					}
-
-					nRadius = TorusParams.nOuterRadius;
-					n = Round( (dStopAngle - dStartAngle) * nRadius / dArcSegLen );
-					if ( n < 8 )
-						n = 8;
-					dThetaStep = (dStopAngle - dStartAngle) / n;
-
-					Add( ptCenter.x + nRadius * cos( dStopAngle ), ptCenter.y + nRadius * sin( dStopAngle ), myFile, pFile );
-					for ( int i = 1; i <= n; i++ )
-					{
-						double dTheta = dStopAngle - i * dThetaStep;
-						Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-					}
-	#endif
+					AddCurve(myFile, pFile, 0, 0, 3, TorusParams.ptCenter.x, TorusParams.ptCenter.y, TorusParams.nInnerRadius, TorusParams.nOuterRadius, TorusParams.dStartAngle, TorusParams.dStopAngle, CCW);
 					break;
 				}
 
@@ -350,37 +229,9 @@ void PolygonToCSV(void)
 				{
 					LPieParams PieParams;
 					LPie_GetParams(pObj, &PieParams);
-					double dStartAngle = PieParams.dStartAngle * M_PI / 180.0;
-					double dStopAngle = PieParams.dStopAngle * M_PI / 180.0;
-					while (dStopAngle < dStartAngle)
-						dStopAngle += 2.0 * M_PI;
-	//				LDialog_MsgBox("Found a pie");
-	//				char msg[200];
-	//				sprintf(msg, "Start = %g Stop = %g", dStartAngle, dStopAngle);
-	//				LDialog_MsgBox(msg);
+					
+					AddCurve(myFile, pFile, 0, 0, 4, PieParams.ptCenter.x, PieParams.ptCenter.y, PieParams.nRadius, 0, PieParams.dStartAngle, PieParams.dStopAngle, CCW);
 
-					// draw the polygon in CW direction, from start/inner
-
-					LPoint ptCenter = PieParams.ptCenter;
-					LCoord nRadius = PieParams.nRadius;
-	#ifdef ALGORITHM_A
-					double dThetaStep = 2*acos(1 - (double)grid.manufacturing_grid_size / nRadius / 10);
-					for ( double dTheta = dStartAngle; dTheta < dStopAngle; dTheta += dThetaStep )
-						Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-					Add( ptCenter.x + nRadius * cos( dStopAngle ), ptCenter.y + nRadius * sin( dStopAngle ), myFile, pFile );
-					Add( PieParams.ptCenter.x, PieParams.ptCenter.y, myFile, pFile );
-	#else
-					int n = Round( (dStopAngle - dStartAngle) * nRadius / dArcSegLen );
-					if ( n < 8 )
-						n = 8;
-					double dThetaStep = (dStopAngle - dStartAngle) / n;
-					for ( int i = 0; i <= n; i++ )
-					{
-						double dTheta = dStartAngle + i * dThetaStep;
-						Add( ptCenter.x + nRadius * cos( dTheta ), ptCenter.y + nRadius * sin( dTheta ), myFile, pFile );
-					}
-					Add( PieParams.ptCenter.x, PieParams.ptCenter.y, myFile, pFile );
-	#endif
 					break;
 				}
 
