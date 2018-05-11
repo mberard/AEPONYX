070fc4b3dd9794565432bcd43456a90adfaaa874
unknown <qgaillot@aeponyx.com>
2018-05-11 14:22:17 -0400

    Import avec curves (testé uniquement avec 90°)

--- PolygonCSV/CSVToPolygon.c
+++ PolygonCSV/CSVToPolygon.c
@@ -1,11 +1,4 @@
 /*******************************************************************************
- *        CODE POUR GESTION DES CURVES HEIGHT EN COURS DE DEVELOPPEMENT        *
- * 									EN STANDBY								   *
- *******************************************************************************/
-
-
-
-/*******************************************************************************
  * Macro Name: CSVToPolygon
  * Creator  : Martin Berard AEPONYX
  *
@@ -21,128 +14,16 @@
 /* Begin -- Uncomment this block if you are using L-Comp. */
 #include <lcomp.h>
 /* End */
+#include <string.h>
 
 #define MAX_POLYGON_SIZE 5000
-#define MAXFLOAT (9.999999999999999e999)
-#ifndef M_PI
-	#define M_PI 3.14159265358979323846
-#endif 
 
-float getAngle(LPoint current, LPoint next, float curveHeight)
-{
-	float angle = 0;
-	// ALWAYS USE COUNTER-CLOCKWISE VERTEX/POLYGON for 90 degres angles
-	LPoint center = LPoint_Set(0,0);
-	if(current.x < next.x)
-		if(current.y < next.y)
-			if(curveHeight < 0)
-				angle = 135;
-			else
-				angle = 315;
-		else			
-			if(curveHeight < 0)
-				angle = 45;
-			else
-				angle = 225;
-	else
-		if(current.y < next.y)
-			if(curveHeight < 0)
-				angle = 225;
-			else
-				angle = 45;
-		else
-			if(curveHeight < 0)
-				angle = 315;
-			else
-				angle = 135;
-
-	return angle;
-}
-
-LPoint getArcCenter(LVertex current, LVertex next, float curveHeight)
-{
-	float dx, dy, coef_IU, dCHx, dCHy, angle;
-	dx = LVertex_GetPoint(next).x - LVertex_GetPoint(current).x;
-	dy = LVertex_GetPoint(next).y - LVertex_GetPoint(current).y;
-
-	//dx_positive = getPositive(dx);
-	//dy_positive = getPositive(dy);
-
-	coef_IU = 1000;
-
-	LPoint chPoint = LPoint_Set( LVertex_GetPoint(current).x + dx/(2.0) , LVertex_GetPoint(current).y + dy/(2.0) ); //center of the 2 points
-
-	curveHeight = curveHeight*coef_IU;
-
-	angle = getAngle(LVertex_GetPoint(current), LVertex_GetPoint(next), curveHeight);
-
-	dCHx = curveHeight*0.707; //replace with cos/sin
-	dCHy = curveHeight*0.707;
-
-	//angle from 0 to 360
-	if(angle<90 && angle>=0)
-		chPoint = LPoint_Set(chPoint.x + dCHx, chPoint.y + dCHy);
-	else if(angle<180 && angle>=90)
-		chPoint = LPoint_Set(chPoint.x - dCHx, chPoint.y + dCHy);
-	else if(angle<270 && angle>=180)
-		chPoint = LPoint_Set(chPoint.x - dCHx, chPoint.y - dCHy);
-	else if(angle<360 && angle>=270)
-		chPoint = LPoint_Set(chPoint.x + dCHx, chPoint.y - dCHy);
-
-	
-	//LDialog_MsgBox(LFormat("dCHx: %f\n", dCHx));
-
-	LPoint A = LVertex_GetPoint(current);
-	LPoint B = chPoint;
-	LPoint C = LVertex_GetPoint(next);
-    LPoint center = LPoint_Set(0,0);
-
-	float yDelta_a = B.y - A.y;
-    float xDelta_a = B.x - A.x;
-    float yDelta_b = C.y - B.y;
-    float xDelta_b = C.x - B.x;
-	float centreX = 0;
-	float centreY = 0;
-
-    float aSlope = yDelta_a/xDelta_a;
-    float bSlope = yDelta_b/xDelta_b;  
-    centreX = (aSlope*bSlope*(A.y - C.y) + bSlope*(A.x + B.x) - aSlope*(B.x + C.x) )/( 2*(bSlope - aSlope) );
-    centreY = -1*(center.x - (A.x + B.x)/2) / aSlope +  (A.y + B.y)/2;
-
-	//LDialog_MsgBox(LFormat("Centre X = %f    Y = %f" , centreX, centreY ));
-
-	center.x = centreX;
-	center.y = centreY;
-
-/*
-	// ALWAYS USE COUNTER-CLOCKWISE VERTEX/POLYGON for 90 degres angles
-	LPoint center = LPoint_Set(0,0);
-	if(LVertex_GetPoint(current).x < LVertex_GetPoint(next).x)
-		if(LVertex_GetPoint(current).y < LVertex_GetPoint(next).y)
-			if(curveHeight < 0)
-				center = LPoint_Set(LVertex_GetPoint(next).x, LVertex_GetPoint(current).y);
-			else
-				center = LPoint_Set(LVertex_GetPoint(current).x, LVertex_GetPoint(next).y);
-		else			
-			if(curveHeight < 0)
-				center = LPoint_Set(LVertex_GetPoint(current).x, LVertex_GetPoint(next).y);
-			else
-				center = LPoint_Set(LVertex_GetPoint(next).x, LVertex_GetPoint(current).y);
-	else
-		if(LVertex_GetPoint(current).y < LVertex_GetPoint(next).y)
-			if(curveHeight < 0)
-				center = LPoint_Set(LVertex_GetPoint(current).x, LVertex_GetPoint(next).y);
-			else
-				center = LPoint_Set(LVertex_GetPoint(next).x, LVertex_GetPoint(current).y);
-		else
-			if(curveHeight < 0)
-				center = LPoint_Set(LVertex_GetPoint(next).x, LVertex_GetPoint(current).y);
-			else
-				center = LPoint_Set(LVertex_GetPoint(current).x, LVertex_GetPoint(next).y);
-*/
-
-	return center;
-}
+typedef struct curve{
+	int hasCurve; //0: no curve, 1: has curve
+	LCoord cx,cy;
+	double r;
+	LArcDirection dir;
+}t_curve;
 
 void CSVToPolygon(void)
 {
@@ -150,22 +31,29 @@ void CSVToPolygon(void)
 	LCell	pCell	=	LCell_GetVisible();
 	LFile	pFile	=	LCell_GetFile(pCell);
 	LLayer pLayer;
-	LCoord XC = 0;
-	LCoord YC = 0;
 	LPoint point_arr[MAX_POLYGON_SIZE]; // could use malloc of LPoint (2 x LCoord or 2x Long or 2x 32bits) for undefined array size or base buffer on linecount
-	float curveHeight_arr[MAX_POLYGON_SIZE];
-	float X,Y,CH;
+	LCoord X,Y,CX,CY,R;
+	LArcDirection DIR;
+
 	char cwd[MAX_TDBFILE_NAME];
 	char sLayerName[MAX_LAYER_NAME];
+
+	char line[256];
+
+	t_curve tcurve_arr[MAX_POLYGON_SIZE];
+
+	LVertex vertex;
+
+	char* token;
+
    	FILE * myFile;
    	int nPoints = 0;
+	int i = 0;
 	int cpt = 0;
 
 	LObject polygon = NULL;
-	LPoint center = LPoint_Set(0,0);
-	LVertex firstVertex = NULL;
 
-	LDialogItem DialogItems[2] = {{ "CSV file","polygon_fillet.csv"}, { "Layer","WGUIDE"}};
+	LDialogItem DialogItems[2] = {{ "CSV file","polygon.csv"}, { "Layer","WGUIDE"}};
 	
 	if (LDialog_MultiLineInputBox("Polygon File",DialogItems,2))
    	{
@@ -191,15 +79,58 @@ void CSVToPolygon(void)
 		else 
 		{
 			LUpi_LogMessage(LFormat("opened polygon file is: %s\n", cwd));
-   			while (!feof(myFile))
-   			{
-    			fscanf(myFile, "%f,%f,%f", &X, &Y, &CH);
-   				XC = LC_Microns(X);
-   				YC = LC_Microns(Y);
-				point_arr[nPoints] = LPoint_Set(XC, YC);
-				curveHeight_arr[nPoints] = CH;
-				nPoints = nPoints+1;	
-			}
+			while(!feof(myFile))
+			{
+				// reads text until newline 
+				fscanf(myFile,"%[^\n]", line);
+				LUpi_LogMessage(LFormat("READ LINE: %s\n", line));
+				cpt=0;
+				for(i=0; i<strlen(line);i++)
+				{
+					if(line[i]==',')
+						cpt++;
+				}
+
+				token = strtok(line, ",");
+
+				LUpi_LogMessage(LFormat("i: %d\n", cpt));
+				if(cpt == 1)
+				{
+					X = LC_Microns(atoi(token));
+					token = strtok(NULL, ",");
+					Y = LC_Microns(atoi(token));
+
+					point_arr[nPoints] = LPoint_Set(X, Y);
+					tcurve_arr[nPoints].hasCurve = 0;
+					nPoints = nPoints+1;
+				}
+				else if(cpt == 5)
+				{
+					X = LC_Microns(atoi(token));
+					token = strtok(NULL, ",");
+					Y = LC_Microns(atoi(token));
+					token = strtok(NULL, ",");
+					CX = LC_Microns(atoi(token));
+					token = strtok(NULL, ",");
+					CY = LC_Microns(atoi(token));
+					token = strtok(NULL, ",");
+					R = LC_Microns(atoi(token));
+					token = strtok(NULL, ",");
+					if(strcmp(token,"CW") == 0)
+						DIR = CW;
+					else
+						DIR = CCW;
+
+					point_arr[nPoints] = LPoint_Set(X, Y);
+					tcurve_arr[nPoints].hasCurve = 1;
+					tcurve_arr[nPoints].cx = CX;
+					tcurve_arr[nPoints].cy = CY;
+					tcurve_arr[nPoints].r = R;
+					tcurve_arr[nPoints].dir = DIR;
+					nPoints = nPoints+1;
+				}
+				fscanf(myFile,"\n");
+			}	
 			fclose(myFile);
 		}
 	}
@@ -207,32 +138,18 @@ void CSVToPolygon(void)
 	//LDialog_MsgBox(LFormat("nPoints = %d." , nPoints ));
 	LUpi_LogMessage(LFormat("nPoints = %d.\n" , nPoints ));
 	polygon=LPolygon_New(pCell, pLayer, point_arr, nPoints);
-
-	firstVertex = LObject_GetVertexList(polygon);
-
-float f1,f2;
-	cpt = 0;
-	// for each vertex of the polygon
-	for(LVertex currentVertex = LObject_GetVertexList(polygon); currentVertex != NULL; currentVertex = LVertex_GetNext(currentVertex))
+	
+	vertex = LObject_GetVertexList(polygon);
+	for(cpt=0; cpt<nPoints; cpt++)
 	{
-		//if there is a curve height for that vertex
-		if(curveHeight_arr[cpt] != 0)
+		if(tcurve_arr[cpt].hasCurve == 1)
 		{
-			//last vertex link to the first one
-			if(LVertex_GetNext(currentVertex) == NULL)
-				center = getArcCenter(currentVertex, firstVertex, curveHeight_arr[cpt]);
-			else
-				center = getArcCenter(currentVertex, LVertex_GetNext(currentVertex), curveHeight_arr[cpt]);
-			//LPoint center = LPoint_Set((LVertex_GetPoint(currentVertex).x+LVertex_GetPoint(LVertex_GetNext(currentVertex)).x)/2, (LVertex_GetPoint(currentVertex).y+LVertex_GetPoint(LVertex_GetNext(currentVertex)).y)/2);
-			//LUpi_LogMessage(LFormat("Centre X: %s\n", center.x));
-			LVertex_AddCurve(polygon, currentVertex, center, CCW);
-			f1 = center.x;
-			f2 = center.y;
-			LUpi_LogMessage(LFormat("Centre X: %f, Y: %f\n", f1, f2));
+			LUpi_LogMessage(LFormat("TRAITEMENT\n" ));
+			LVertex_AddCurve(polygon, vertex, LPoint_Set(tcurve_arr[cpt].cx, tcurve_arr[cpt].cy), tcurve_arr[cpt].dir);
 		}
-		cpt++;
+		vertex = LVertex_GetNext(vertex);
 	}
-	//LUpi_LogMessage(LFormat("cos 90: %s\n", LVertex_GetPoint(currentVertex).x));
+
 	//LCell_MakeVisible ( pCell );
 	LDisplay_Refresh();
 }
