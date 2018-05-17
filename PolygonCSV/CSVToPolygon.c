11198ecc3441cce98ce258cdbd5a46fbd3be68d4
unknown <qgaillot@aeponyx.com>
2018-05-17 11:46:27 -0400

    Debut import nouvelle forme

--- PolygonCSV/CSVToPolygon.c
+++ PolygonCSV/CSVToPolygon.c
@@ -18,28 +18,123 @@
 
 #define MAX_POLYGON_SIZE 5000
 #define MAX_PORT_NAME_LENGTH 200
+#define MAX_LABEL_TEXT_LENGTH 200
 #define MAX_LENGTH_PATH 256
 const char saveFile[] = "importPath.txt";
 
 typedef struct shape{
-	LShape shapeType;
+	LShapeType shapeType;
 	LGeomType geomType;
-	DPoint point_arr[MAX_POLYGON_SIZE]; // could use malloc of LPoint (2 x LCoord or 2x Long or 2x 32bits) for undefined array size or base buffer on linecount
-	int nPoints = 0;
-	DPoint XY;
-	double r = 0;
-	double r2 = 0;
-	double startAngle = 0; 
-	double stopAngle = 0;
-	LArcDirection dir;
-	char* text = "";
+	LPoint point_arr[MAX_POLYGON_SIZE]; // could use malloc of LPoint (2 x LCoord or 2x Long or 2x 32bits) for undefined array size or base buffer on linecount
+	int nPoints;
+	LPoint XY[MAX_POLYGON_SIZE];
+	double r;
+	double r2;
+	double startAngle; 
+	double stopAngle;
+	LArcDirection dir[MAX_POLYGON_SIZE];
+	char text[MAX_PORT_NAME_LENGTH];
 	LWireConfig wireConfig;
 }t_shape;
 
+typedef struct label{
+	char text[MAX_LABEL_TEXT_LENGTH];
+	double x, y;
+	double textSize;
+	int textAlignment;
+}t_label;
+
 LShapeType GetShapeType(char* str)
 {
+	LShapeType shape;
+	if(strcmp(str,"LBox")==0)
+		shape = LBox;
+	else if(strcmp(str,"LCircle")==0)
+		shape = LCircle;
+	else if(strcmp(str,"LWire")==0)
+		shape = LWire;
+	else if(strcmp(str,"LPolygon")==0)
+		shape = LPolygon;
+	else if(strcmp(str,"LTorus")==0)
+		shape = LTorus;
+	else if(strcmp(str,"LPie")==0)
+		shape = LPie;
+	else if(strcmp(str,"LOtherObject")==0)
+		shape = LOtherObject;
+	else if(strcmp(str,"LObjInstance")==0)
+		shape = LObjInstance;
+	else if(strcmp(str,"LObjPort")==0)
+		shape = LObjPort;
+	else if(strcmp(str,"LObjRuler")==0)
+		shape = LObjRuler;
 	
 	return shape;
+}
+
+LGeomType GetGeometryType(char* str)
+{
+	LGeomType geom;
+	if(strcmp(str,"LOrthogonal")==0)
+		geom = LOrthogonal;
+	else if(strcmp(str,"LFortyFive")==0)
+		geom = LFortyFive;
+	else if(strcmp(str,"LAllAngle")==0)
+		geom = LAllAngle;
+	else if(strcmp(str,"LCurved")==0)
+		geom = LCurved;
+	else if(strcmp(str,"LNonGeometric")==0)
+		geom = LNonGeometric;
+
+	return geom;
+}
+
+LArcDirection GetArcDirection(char* str)
+{
+	LUpi_LogMessage(LFormat("str %s\n", str ));
+	LUpi_LogMessage(LFormat("coucou\n"));
+	LArcDirection dir;
+	if(strcmp(str,"CW")==0)
+		dir = CW;
+	else if(strcmp(str,"CCW")==0)
+		dir = CCW;
+
+	return dir;
+}
+
+LJoinType GetJoinType(char* str)
+{
+	LUpi_LogMessage(LFormat("str %s\n", str ));
+	LUpi_LogMessage(LFormat("coucou\n"));
+	LJoinType join;
+	if(strcmp(str,"LJoinMiter")==0)
+		join = LJoinMiter;
+	else if(strcmp(str,"LJoinRound")==0)
+		join = LJoinRound;
+	else if(strcmp(str,"LJoinBevel")==0)
+		join = LJoinBevel;
+	else if(strcmp(str,"LJoinLayout")==0)
+		join = LJoinLayout;
+
+	return join;
+}
+
+LCapType GetCapType(char* str)
+{
+	
+	LCapType cap;
+	
+	LUpi_LogMessage(LFormat("str %s\n", str ));
+	if(str != NULL)
+	{
+		if(strcmp(str,"LCapButt")==0)
+			cap = LCapButt;
+		else if(strcmp(str,"LCapRound")==0)
+			cap = LCapRound;
+		else if(strcmp(str,"LCapExtend")==0)
+			cap = LCapExtend;
+	}
+	
+	return cap;
 }
 
 void CSVToPolygon(void)
@@ -58,6 +153,13 @@ void CSVToPolygon(void)
 	char line[256];
 
 	t_shape shape;
+	shape.nPoints = 0;
+	shape.r = 0;
+	shape.r2 = 0;
+	shape.startAngle = 0; 
+	shape.stopAngle = 0;
+
+	t_label labelStruct;
 
 	LVertex vertex;
 
@@ -135,173 +237,140 @@ void CSVToPolygon(void)
 
 				if(cpt == 3) //point
 				{
-					shape.shapeType = GetShapeType();
+				LUpi_LogMessage(LFormat("BEGIN\n" ));
+					shape.shapeType = GetShapeType(token);
+				LUpi_LogMessage(LFormat("%s STORED\n",shape.shapeType ));
 					token = strtok(NULL, ","); //next part
-					shape.geomType = GetGeometryType();
+					shape.geomType = GetGeometryType(token);
+				LUpi_LogMessage(LFormat("%s STORED\n",shape.geomType ));
 					token = strtok(NULL, ",");
-					X = LFile_MicronsToIntU(pFile,atoi(token));
+					shape.point_arr[shape.nPoints].x = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					Y = LFile_MicronsToIntU(pFile,atoi(token));
-
-					point_arr[nPoints] = LPoint_Set(X, Y);
-					tcurve_arr[nPoints].typeCurve = 0;
-					nPoints = nPoints+1;
+					shape.point_arr[shape.nPoints].y = LFile_MicronsToIntU(pFile,atof(token));
+					shape.nPoints = shape.nPoints+1;
+					
+					LUpi_LogMessage(LFormat("%s STORED\n",shape.geomType ));
 				}
-				else if(cpt == 9) //point with a curve or circle or pie or torus
+				else if(cpt == 15)
 				{
-					X = LFile_MicronsToIntU(pFile,atoi(token));
+				LUpi_LogMessage(LFormat("BEGIN\n" ));
+					shape.shapeType = GetShapeType(token);
+					token = strtok(NULL, ","); //next part
+					shape.geomType = GetGeometryType(token);
 					token = strtok(NULL, ",");
-					Y = LFile_MicronsToIntU(pFile,atoi(token));
+					shape.point_arr[shape.nPoints].x = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					TYPE = atoi(token);
+					shape.point_arr[shape.nPoints].y = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					CX = LFile_MicronsToIntU(pFile,atoi(token));
+					shape.XY[shape.nPoints].x = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					CY = LFile_MicronsToIntU(pFile,atoi(token));
+					shape.XY[shape.nPoints].y = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					R = LFile_MicronsToIntU(pFile,atoi(token));
+					shape.r = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					R2 = LFile_MicronsToIntU(pFile,atoi(token));
+					shape.r2 = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					START = atoi(token);
+					shape.startAngle = atof(token);
 					token = strtok(NULL, ",");
-					STOP = atoi(token);
+					shape.stopAngle = atof(token);
 					token = strtok(NULL, ",");
-					if(strcmp(token,"CW") == 0)
-						DIR = CW;
-					else
-						DIR = CCW;
+					shape.dir[shape.nPoints] = GetArcDirection(token);
+					token = strtok(NULL, ",");
+					strcpy(shape.text, token);
+					token = strtok(NULL, ",");
+					shape.wireConfig.width = LFile_MicronsToIntU(pFile,atof(token));
+					token = strtok(NULL, ",");
+					shape.wireConfig.join = GetJoinType(token);
+					token = strtok(NULL, ",");
+					shape.wireConfig.cap = GetCapType(token);
+					token = strtok(NULL, ",");
+					shape.wireConfig.miter_angle = atof(token);
 
-					point_arr[nPoints] = LPoint_Set(X, Y);
-					tcurve_arr[nPoints].typeCurve = TYPE;
-					tcurve_arr[nPoints].cx = CX;
-					tcurve_arr[nPoints].cy = CY;
-					tcurve_arr[nPoints].r = R;
-					tcurve_arr[nPoints].r2 = R2;
-					tcurve_arr[nPoints].startAngle = START;
-					tcurve_arr[nPoints].stopAngle = STOP;
-					tcurve_arr[nPoints].dir = DIR;
-					nPoints = nPoints+1;
+					shape.nPoints = shape.nPoints+1;
 				}
 				else if(cpt == 5) //port or label
 				{
-					portlabel.x0 = LFile_MicronsToIntU(pFile,atoi(token));
+					labelStruct.x = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					portlabel.y0 = LFile_MicronsToIntU(pFile,atoi(token));
+					labelStruct.y = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					portlabel.type = atoi(token);
+					labelStruct.textSize = LFile_MicronsToIntU(pFile,atof(token));
 					token = strtok(NULL, ",");
-					portlabel.x1 = LFile_MicronsToIntU(pFile,atoi(token));
+					labelStruct.textAlignment = atoi(token);
 					token = strtok(NULL, ",");
-					if (portlabel.type ==5)
-						portlabel.y1 = LFile_MicronsToIntU(pFile,atoi(token));
-					else
-						portlabel.y1 = (double)atoi(token);
-					token = strtok(NULL, ",");
-					strcpy(portlabel.text, token);
-				}
-				else if(cpt == 6) //wire
-				{
-					X = LFile_MicronsToIntU(pFile,atoi(token));
-					token = strtok(NULL, ",");
-					Y = LFile_MicronsToIntU(pFile,atoi(token));
-					token = strtok(NULL, ",");
-					TYPE = atoi(token);
-					token = strtok(NULL, ",");
-					wire_config.width = LFile_MicronsToIntU(pFile,atoi(token));
-					token = strtok(NULL, ",");
-					if(strcmp(token,"LJoinMiter")==0)
-						wire_config.join = LJoinMiter;
-					else if(strcmp(token,"LJoinRound")==0)
-						wire_config.join = LJoinRound;
-					else if(strcmp(token,"LJoinBevel")==0)
-						wire_config.join = LJoinBevel;
-					else if(strcmp(token,"LJoinLayout")==0)
-						wire_config.join = LJoinLayout;
-
-					token = strtok(NULL, ",");
-					if(strcmp(token,"LCapButt")==0)
-						wire_config.cap = LCapButt;
-					else if(strcmp(token,"LCapRound")==0)
-						wire_config.cap = LCapRound;
-					else if(strcmp(token,"LCapExtend")==0)
-						wire_config.cap = LCapExtend;
-
-					token = strtok(NULL, ",");
-					wire_config.miter_angle = LFile_MicronsToIntU(pFile,atoi(token));
-
-					point_arr[nPoints] = LPoint_Set(X, Y);
-					nPoints++;
+					strcpy(labelStruct.text, token);
 				}
 				fscanf(myFile,"\n"); //got o the next line
 			}	
 			fclose(myFile);
 		}
-		if(cpt == 1 || cpt == 9) //poygon with or without curve, circle, pie, torus
+
+
+
+		if(cpt == 3 || cpt == 17) //poygon with or without curve, circle, pie, torus
 		{
-			polygon=LPolygon_New(pCell, pLayer, point_arr, nPoints); //create the polygon that will be modified
+			polygon=LPolygon_New(pCell, pLayer, shape.point_arr, shape.nPoints); //create the polygon that will be modified
 			LUpi_LogMessage(LFormat("POLYGON CREATED\n" ));
 			
 			vertex = LObject_GetVertexList(polygon);
-			for(cpt=0; cpt<nPoints; cpt++) //for each vertex
+			for(cpt=0; cpt<shape.nPoints; cpt++) //for each vertex
 			{
-				if(tcurve_arr[cpt].typeCurve == 1) //is a curve
+				switch(shape.shapeType)
 				{
-					LUpi_LogMessage(LFormat("CURVE\n" ));
-					LVertex_AddCurve(polygon, vertex, LPoint_Set(tcurve_arr[cpt].cx, tcurve_arr[cpt].cy), tcurve_arr[cpt].dir);
-				}
-				else if(tcurve_arr[cpt].typeCurve == 2) //is a circle
-				{
-					LUpi_LogMessage(LFormat("CIRCLE\n" ));
-					polygon = LCircle_New( pCell, pLayer, LPoint_Set(tcurve_arr[cpt].cx, tcurve_arr[cpt].cy), tcurve_arr[cpt].r );
-				}
-				else if(tcurve_arr[cpt].typeCurve == 3) //is a torus
-				{
-					LUpi_LogMessage(LFormat("TORUS\n" ));
-					LTorusParams p;
-					p.ptCenter = LPoint_Set(tcurve_arr[cpt].cx, tcurve_arr[cpt].cy);
-					p.nInnerRadius = tcurve_arr[cpt].r;
-					p.nOuterRadius = tcurve_arr[cpt].r2;
-					p.dStartAngle = tcurve_arr[cpt].startAngle;
-					p.dStopAngle = tcurve_arr[cpt].stopAngle;
+					case LBox:
+						LUpi_LogMessage(LFormat("BOX\n" ));
+						polygon = LBox_New( pCell, pLayer, shape.point_arr[cpt].x, shape.point_arr[cpt].y, shape.XY[cpt].x, shape.XY[cpt].y );
+						break;
+					case LCircle:
+						LUpi_LogMessage(LFormat("CIRCLE\n" ));
+						polygon = LCircle_New( pCell, pLayer, shape.XY[cpt], shape.r );
+						break;
+					case LWire:
+						LUpi_LogMessage(LFormat("WIRE CREATED\n"));
+						polygon = LWire_New( pCell, pLayer, &shape.wireConfig, LSetWireAll, shape.point_arr, shape.nPoints );
+						break;
+					case LPolygon:
+						if(shape.geomType == LCurved)
+						{
+							LUpi_LogMessage(LFormat("ADDING A CURVE TO THE POLYGON\n" ));
+							LVertex_AddCurve(polygon, vertex, shape.XY[cpt], shape.dir[cpt]);
+						}
+						break;
+					case LTorus:
+						LUpi_LogMessage(LFormat("TORUS\n" ));
+						LTorusParams tp;
+						tp.ptCenter = shape.XY[cpt];
+						tp.nInnerRadius = shape.r;
+						tp.nOuterRadius = shape.r2;
+						tp.dStartAngle = shape.startAngle;
+						tp.dStopAngle = shape.stopAngle;
 
-					polygon = LTorus_CreateNew( pCell, pLayer, &p );
-				}
-				else if(tcurve_arr[cpt].typeCurve == 4) //is a pie
-				{
-					LUpi_LogMessage(LFormat("PIE\n" ));
-					LPieParams p;
-					p.ptCenter = LPoint_Set(tcurve_arr[cpt].cx, tcurve_arr[cpt].cy);
-					p.nRadius = tcurve_arr[cpt].r;
-					p.dStartAngle = tcurve_arr[cpt].startAngle;
-					p.dStopAngle = tcurve_arr[cpt].stopAngle;
+						polygon = LTorus_CreateNew( pCell, pLayer, &tp );
+						break;
+					case LPie:
+						LUpi_LogMessage(LFormat("PIE\n" ));
+						LPieParams pp;
+						pp.ptCenter = shape.XY[cpt];
+						pp.nRadius = shape.r;
+						pp.dStartAngle = shape.startAngle;
+						pp.dStopAngle = shape.stopAngle;
 
-					polygon = LPie_CreateNew( pCell, pLayer, &p );
+						polygon = LPie_CreateNew( pCell, pLayer, &pp );
+						break;
+					//PORT
+					default:
+						LUpi_LogMessage(LFormat("Object shape not found\n" ));
 				}
 				vertex = LVertex_GetNext(vertex);
-			}
+			}			
 		}
-		else if(cpt == 5) //for port or label
+		else if(cpt == 5) //for label
 		{
-			if(portlabel.type == 5)
-			{
-				LUpi_LogMessage(LFormat("PORT CREATED\n"));
-				polygon = LPort_New( pCell, pLayer, portlabel.text, portlabel.x0, portlabel.y0, portlabel.x1, portlabel.y1 );
-			}
-			else if(portlabel.type == 7)
-			{
-				LUpi_LogMessage(LFormat("LABEL CREATED\n"));
-				LLabel label;
-				label = LLabel_New( pCell, pLayer, portlabel.text, portlabel.x0, portlabel.y0 );
-				LLabel_SetTextSize( pCell, label, portlabel.x1 );
-				LLabel_SetTextAlignment( pCell, label, (int)portlabel.y1 );
-			}
-			
-		}
-		else if(cpt == 6) //for wire
-		{
-			LUpi_LogMessage(LFormat("WIRE CREATED\n"));
-			polygon = LWire_New( pCell, pLayer, &wire_config, LSetWireAll, point_arr, nPoints );
+			LUpi_LogMessage(LFormat("LABEL CREATED\n"));
+			LLabel label;
+			label = LLabel_New( pCell, pLayer, labelStruct.text, labelStruct.x, labelStruct.y );
+			LLabel_SetTextSize( pCell, label, labelStruct.textSize );
+			LLabel_SetTextAlignment( pCell, label, labelStruct.textAlignment );
 		}
 	}
 
