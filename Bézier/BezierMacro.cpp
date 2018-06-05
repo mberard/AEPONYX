6b1ff3ed29b1886ff8de956bb52de63847c2ff6f
unknown <qgaillot@aeponyx.com>
2018-06-05 09:48:33 -0400

    calcul points de control

--- Bézier/BezierMacro.cpp
+++ Bézier/BezierMacro.cpp
@@ -9,8 +9,9 @@
 #include <ldata.h>
 #include <unistd.h> //getcwd
 
-
 #define EXCLUDE_LEDIT_LEGACY_UPI //This statement make the C language macros, which are now superseded by C++ functions, unavailable.
+
+#define BEZIER_PARAM 0.45
 
 extern "C" {
     #include "DubinsPoint.hpp"
@@ -23,7 +24,42 @@ extern "C" {
 
 void BezierMacro()
 {
+    DubinsPoint start, end;
+    LPoint controlStart, controlEnd;
 
+    LCell	pCell	=	LCell_GetVisible();
+	LFile	pFile	=	LCell_GetFile(pCell);
+    LLayer pLayer;
+
+    double xStart, yStart, xEnd, yEnd;
+    double angleStart, angleEnd;
+    double coef;
+    double distX, distY;
+
+    start.SetPoint(0,0,pFile);
+    start.SetAngleDegre(90);
+    end.SetPoint(1,1,pFile);
+    end.SetAngleDegre(90);
+
+    xStart = start.GetPoint().x;
+    yStart = start.GetPoint().y;
+    xEnd = end.GetPoint().x;
+    yEnd = end.GetPoint().y;
+    angleStart = start.GetAngleRadian();
+    angleEnd = end.GetAngleRadian();
+
+    coef = 1 - BEZIER_PARAM;
+
+    distX = xEnd - xStart;
+    distY = yEnd - yStart;
+
+    controlStart.x = (LCoord) ( xStart + distX * coef * cos(angleStart) );
+    controlStart.y =  (LCoord) (yStart + distY * coef * sin(angleStart) );
+    controlEnd.x = (LCoord) ( xEnd + distX * coef * cos(angleEnd + M_PI) );
+    controlEnd.y = (LCoord) ( yEnd + distY * coef * sin(angleEnd + M_PI) );
+
+    LUpi_LogMessage(LFormat("controlStart %ld %ld\n", controlStart.x, controlStart.y));
+    LUpi_LogMessage(LFormat("controlEnd %ld %ld\n", controlEnd.x, controlEnd.y));
 }
 
 int UPI_Entry_Point(void)
