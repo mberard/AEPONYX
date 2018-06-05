5586f92e693ff12c53c02c7930fd469eeabad343
unknown <qgaillot@aeponyx.com>
2018-06-05 09:10:18 -0400

    Début Bezier macro

---
+++ Bézier/BezierMacro.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <string>
+
+#include <ldata.h>
+#include <unistd.h> //getcwd
+
+
+#define EXCLUDE_LEDIT_LEGACY_UPI //This statement make the C language macros, which are now superseded by C++ functions, unavailable.
+
+extern "C" {
+    void BezierMacro(void);
+	int UPI_Entry_Point(void);
+}
+
+void BezierMacro()
+{
+
+}
+
+int UPI_Entry_Point(void)
+{
+	LMacro_BindToMenuAndHotKey_v9_30("Tools", "F2" /*hotkey*/, "Bezier Macro", "BezierMacro", NULL /*hotkey category*/);
+	return 1;
+}
