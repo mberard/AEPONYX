71799e7e6d89e98af0e5e7cfe75e8a6fb449937e
unknown <qgaillot@aeponyx.com>
2018-07-06 13:56:11 -0400

    Suppression de la sauvegarde automatique de backup

--- TorusFillet/TorusFilletMacro.c
+++ TorusFillet/TorusFilletMacro.c
@@ -267,9 +267,6 @@ void AATorusFillet(void)
     
     LUpi_LogMessage("\n\n\n\n\nSTART MACRO\n");
 
-    if ( LStatusOK != LFile_SaveAs( LFile_GetVisible(), "LastBackupBeforeTorrusFilletMacro", LTdbFile))
-        LDialog_AlertBox( "Failed to save new copy of current file" );
-
     if(LSelection_GetList() == NULL) //if no selection made
 	{
         LUpi_LogMessage("No selection were made\n");
